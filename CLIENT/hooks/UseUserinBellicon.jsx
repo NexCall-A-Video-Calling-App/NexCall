961b1ce5dc55c5c387efc2fdfd7cb346ec03e5b4
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-19 20:05:18 +0600

    bell icon user custome adata added done

--- CLIENT/hooks/UseUserinBellicon.jsx
+++ CLIENT/hooks/UseUserinBellicon.jsx
@@ -2,10 +2,25 @@
 
 
 import React from 'react'
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+  } from '@tanstack/react-query'
 
 function UseUserinBellicon() {
 
-    
+    const {isLoading,error,refetch,data:belIconUserData=[] }= useQuery({
+
+        queryKey:['userdata'],
+        queryFn: async () =>{
+            const res = await fetch('http://localhost:5000/users')
+            return res;
+        }
+
+    })
 
 
 
@@ -13,14 +28,7 @@ function UseUserinBellicon() {
 
 
 
-  return (
-    <div>
-
-
-
-
-    </div>
-  )
+  return {isLoading,error,refetch,belIconUserData}
 }
 
 export default UseUserinBellicon
