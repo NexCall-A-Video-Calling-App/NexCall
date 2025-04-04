168274aabb83f64c187ab983862b9fa7317672a6
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-04 19:27:11 +0600

    meeting page add on  sigin page

--- CLIENT/src/pages/SignIn/SignIn.jsx
+++ CLIENT/src/pages/SignIn/SignIn.jsx
@@ -19,6 +19,7 @@ const SignIn = () => {
             .then((result) => {
                 console.log(result.user);
                 // navigate('/dashboard')
+                navigate('/meeting-page')
 
                 reset();
             })
