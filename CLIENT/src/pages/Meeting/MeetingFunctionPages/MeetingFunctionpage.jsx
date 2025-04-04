b5fa0fadaa15c68b73b7310fa4ae72db6a01cb53
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-04 19:49:47 +0600

    time stae added on inside function

--- CLIENT/src/pages/Meeting/MeetingFunctionPages/MeetingFunctionpage.jsx
+++ CLIENT/src/pages/Meeting/MeetingFunctionPages/MeetingFunctionpage.jsx
@@ -1,6 +1,6 @@
 
 
-import React from 'react'
+
 import React, { useEffect, useState } from 'react'
 import { GoDeviceCameraVideo } from "react-icons/go";
 import { Gi3dGlasses, GiTimeTrap } from "react-icons/gi";
@@ -10,6 +10,9 @@ import { BsFillCameraReelsFill } from "react-icons/bs"
 
 const MeetingFunctionpage = () => {
     // main function go under navbar 
+
+    const [ time , settime ] = useState("");
+    const [ fullTime , setfullTime ] = useState("");
   return (
     <div>
       
