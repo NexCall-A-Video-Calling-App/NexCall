8401631c079eee1396c9f806d557246d0b4bd445
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-04 19:33:56 +0600

    links div added on meetingnavbar page

--- CLIENT/src/pages/Meeting/MeetingNavbar/MeetingNavbar.jsx
+++ CLIENT/src/pages/Meeting/MeetingNavbar/MeetingNavbar.jsx
@@ -2,6 +2,7 @@
 
 
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { IoMdHome } from "react-icons/io";
 import { IoChatboxEllipses, IoContract } from "react-icons/io5";
 import { MdContactMail } from "react-icons/md";
@@ -9,6 +10,36 @@ import { SiGoogleclassroom } from "react-icons/si";
 import { IoSearchOutline } from "react-icons/io5";
 
 function MeetingNavbar() {
+
+    const links = (
+        <div className="md:flex">
+          <li>
+            <Link className="md:flex md:flex-col  md:gap-0 text-gray-200 font-semibold">
+              Home <IoMdHome />
+            </Link>
+          </li>
+    
+          <li>
+            <Link  className="md:flex md:flex-col  md:gap-0 text-gray-600 font-semibold">
+              Chat <IoChatboxEllipses />
+            </Link>
+          </li>
+    
+          <li>
+            <Link className="md:flex md:flex-col  md:gap-0 text-gray-600 font-semibold">
+              Meeting <SiGoogleclassroom />
+            </Link>
+          </li>
+    
+          <li>
+            <Link className="md:flex md:flex-col  md:gap-0 text-gray-600 font-semibold">
+              Contact <MdContactMail />
+            </Link>
+          </li>
+        </div>
+      );
+
+
   return (
     <div>
 
