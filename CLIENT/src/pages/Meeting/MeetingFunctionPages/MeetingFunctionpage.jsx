c5e0034c883e6efd7b101851b82e65f44a106d25
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-04 19:45:28 +0600

    meeting function page added done

--- CLIENT/src/pages/Meeting/MeetingFunctionPages/MeetingFunctionpage.jsx
+++ CLIENT/src/pages/Meeting/MeetingFunctionPages/MeetingFunctionpage.jsx
@@ -6,6 +6,7 @@ const MeetingFunctionpage = () => {
   return (
     <div>
       
+      
     </div>
   )
 }
--- CLIENT/src/pages/Meeting/MeetingLayouts/MeetingLayoutes.jsx
+++ CLIENT/src/pages/Meeting/MeetingLayouts/MeetingLayoutes.jsx
@@ -4,12 +4,14 @@
 
 import React from 'react'
 import MeetingNavbar from '../MeetingNavbar/MeetingNavbar'
+import MeetingFunctionpage from '../MeetingFunctionPages/MeetingFunctionpage'
 
 function MeetingLayoutes() {
   return (
     <div>
         {/* all components herer */}
         <MeetingNavbar/>
+        <MeetingFunctionpage/>
 
 
     </div>
