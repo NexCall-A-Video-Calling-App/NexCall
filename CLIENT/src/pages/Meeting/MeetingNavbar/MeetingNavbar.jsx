e2cbdd9688a8c2254b1bdbbbd94cd50254d5d6bd
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-04 19:32:45 +0600

    all icon import on meeting navbar page

--- CLIENT/src/pages/Meeting/MeetingNavbar/MeetingNavbar.jsx
+++ CLIENT/src/pages/Meeting/MeetingNavbar/MeetingNavbar.jsx
@@ -2,11 +2,16 @@
 
 
 import React from 'react'
+import { IoMdHome } from "react-icons/io";
+import { IoChatboxEllipses, IoContract } from "react-icons/io5";
+import { MdContactMail } from "react-icons/md";
+import { SiGoogleclassroom } from "react-icons/si";
+import { IoSearchOutline } from "react-icons/io5";
 
 function MeetingNavbar() {
   return (
     <div>
-        
+
 
 
     </div>
