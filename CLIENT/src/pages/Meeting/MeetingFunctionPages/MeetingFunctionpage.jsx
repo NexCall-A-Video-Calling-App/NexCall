8343bc0c33d960178f6c7ea803627f3788856d73
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-04 19:48:42 +0600

    all icon import on meeting-function page

--- CLIENT/src/pages/Meeting/MeetingFunctionPages/MeetingFunctionpage.jsx
+++ CLIENT/src/pages/Meeting/MeetingFunctionPages/MeetingFunctionpage.jsx
@@ -1,6 +1,12 @@
 
 
 import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GoDeviceCameraVideo } from "react-icons/go";
+import { Gi3dGlasses, GiTimeTrap } from "react-icons/gi";
+import { IoPersonAddSharp } from "react-icons/io5";
+import { BsFillCameraReelsFill } from "react-icons/bs"
+
 
 const MeetingFunctionpage = () => {
     // main function go under navbar 
