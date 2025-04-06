be64bbbe70a0193c9f0c4bfef195b270f10aed9a
masud2005 <masud20012005@gmail.com>
2025-04-06 22:50:08 +0600

    fix: restore senderName to prevent 'Unknown' label

--- SERVER/index.js
+++ SERVER/index.js
@@ -63,7 +63,7 @@ io.on("connection", (socket) => {
             receiverName,
             timestamp: new Date()
         };
-        io.to(room).emit("receiveMessage", { sender: socket.id, photo, message });
+        io.to(room).emit("receiveMessage", { sender: socket.id, senderName, photo, message });
 
         // Save the message to the messages collection
         const messagesCollection = client.db("NexCall").collection('messages');
