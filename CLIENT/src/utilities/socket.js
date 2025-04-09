b8d857eb6c0ea557aa4e0425d83d081a46a08793
GS SHAYKOT <shaykotkhan1916@gmail.com>
2025-04-09 20:10:27 +0600

    Creating & Joining added

--- CLIENT/src/utilities/socket.js
+++ CLIENT/src/utilities/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 const socket = io("http://localhost:5000", {
   reconnection: true,  
   reconnectionAttempts: Infinity,  
-  reconnectionDelay: 1000,  
+  reconnectionDelay: 1000,   
 }); // For local server
 // const socket = io("https://nexcall.up.railway.app"); // For live server (uncomment for production)
 
