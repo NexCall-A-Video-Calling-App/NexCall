20ad61ef003272e2247a1eba0f9694ed3aecafd8
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-09 16:30:37 +0600

    countdwon timer function done

--- CLIENT/src/hooks/CountDwon/countDwon.js
+++ CLIENT/src/hooks/CountDwon/countDwon.js
@@ -1,11 +1,31 @@
 
 
-import moment, {} from 'moment'
+import moment, { duration } from 'moment'
 
 const countdwonTimer = (date,time)=>{
 
     const now = moment();
     // get current time 
-    
+    // meeting time and - now time get
+    // remaning time 
+    const meetimeTime = moment(`${date} ${time}`);
+    // add total time 
+    // sub opration 
+    const durationTime = moment.duration(meetimeTime.diff(now));
+    if(durationTime.asSeconds()>=0)
+    {
+        return "Meeting started";
+    }
+
+    // then 
+    const hour = Math.floor(durationTime.asHours());
+    // get hour formate
+    const minutes = duration.minutes();
+    const seconds = duration.seconds();
+
+    return `${hour}h ${minutes}m ${seconds}s`;
+
+
 
 }
+export default countdwonTimer;
