c23d6c38a42058a16e4e03c95d040132b8eea2f2
SumonaYeasmin <sumonayeasmin570@gmail.com>
2025-04-09 11:25:36 +0600

    create UI for reset password page with email input

--- CLIENT/src/pages/ForgotPassword/ForgotPassword.jsx
+++ CLIENT/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
 
+// import { useForm } from "react-hook-form";
 const ForgotPassword = () => {
+    // const { register, handleSubmit, formState: { errors } } = useForm();
     return (
-        <div>
-            
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+            <h2 className="text-2xl font-bold text-center mb-4">Reset Password</h2>
+            <p className="text-sm text-gray-600 text-center mb-6">Enter your email address to receive a password reset link.</p>
+           
         </div>
+    </div>
     );
 };
 
