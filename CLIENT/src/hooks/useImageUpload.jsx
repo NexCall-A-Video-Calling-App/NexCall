e486a2493013c60f0b0897ec706979f1699249fc
SumonaYeasmin <sumonayeasmin570@gmail.com>
2025-04-09 17:09:44 +0600

    implement image upload service with imgBB

--- CLIENT/src/hooks/useImageUpload.jsx
+++ CLIENT/src/hooks/useImageUpload.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import axios from 'axios';
 
 const useImageUpload = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const uploadImage = async (imageFile) => {
+        const formData = new FormData();
+        formData.append('image', imageFile);
+        const apiKey = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+
+        try {
+            const res = await axios.post(`https://api.imgbb.com/1/upload?key=${apiKey}`, formData);
+            console.log(res.data.data.display_url);
+            return res.data.data.display_url;
+        } catch (error) {
+            console.error("Image upload failed", error);
+            return null;
+        }
+    };
+
+    return { uploadImage };
 };
 
 export default useImageUpload;
