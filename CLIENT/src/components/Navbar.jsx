d88d6523f0db17d0e4d75fa2af1265bae94b80d7
masud2005 <masud20012005@gmail.com>
2025-04-12 14:13:43 +0600

    add container class with nav content

--- CLIENT/src/components/Navbar.jsx
+++ CLIENT/src/components/Navbar.jsx
@@ -35,81 +35,83 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100 shadow-sm w-full px-4 lg:px-12 md:px-10 sm:px-6 fixed top-0 z-50">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className=" lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="bg-base-100 shadow-sm w-full fixed top-0 z-50">
+      <div className="container mx-auto navbar px-2 ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className=" lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-md dropdown-content bg-base-100 rounded-box z-1 mt-3 w-44 p-2 shadow"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              {links}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            {links}
-          </ul>
-        </div>
 
-        <Link
-          to={"/"}
-          className="flex items-center hover:underline hover:text-blue-700 text-xl"
-        >
-          <img
-            className="w-10 h-10 object-cover"
-            src={logo}
-            alt="NexCall Logo"
-          />
-          <span>NexCall</span>
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-base">{links}</ul>
-      </div>
-      <div className="navbar-end">
-        <div className="dropdown dropdown-end">
-          {
-            user ? (
-              <>
-                <div className="dropdown dropdown-end">
-                  <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                      <img src={user?.photoURL} referrerPolicy="no-referrer" className="w-8 h-8 rounded-full" alt="" />
+          <Link
+            to={"/"}
+            className="flex items-center hover:underline hover:text-blue-700 text-xl"
+          >
+            <img
+              className="w-10 h-10 object-cover"
+              src={logo}
+              alt="NexCall Logo"
+            />
+            <span>NexCall</span>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-base">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            {
+              user ? (
+                <>
+                  <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                      <div className="w-10 rounded-full">
+                        <img src={user?.photoURL} referrerPolicy="no-referrer" className="w-8 h-8 rounded-full" alt="" />
+                      </div>
                     </div>
+                    <ul
+                      tabIndex={0}
+                      className="menu menu-md dropdown-content bg-base-100 rounded-box z-1 mt-3 w-44 p-2 shadow">
+                      <li>
+                        <Link to={'/profile'}>Profile</Link>
+                      </li>
+                      <li>
+                        <Link to={'/meeting'}>Dashboard</Link>
+                      </li>
+                      <li>
+                        <button onClick={handleLogout}>Logout</button>
+                      </li>
+                    </ul>
                   </div>
-                  <ul
-                    tabIndex={0}
-                    className="menu menu-md dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li>
-                      <Link to={'/profile'}>Profile</Link>
-                    </li>
-                    <li>
-                      <Link to={'/meeting'}>Dashboard</Link>
-                    </li>
-                    <li>
-                      <button onClick={handleLogout}>Logout</button>
-                    </li>
-                  </ul>
-                </div>
-              </>
-            ) : (
-              <Link to="/sign-up" className="btn">
-                SignUp
-              </Link>
-            )
-          }
+                </>
+              ) : (
+                <Link to="/sign-up" className="btn">
+                  SignUp
+                </Link>
+              )
+            }
+          </div>
         </div>
       </div>
     </div>
