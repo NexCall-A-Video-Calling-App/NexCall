fd74d568ad10179ed03ed4d0be30919850150130
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-22 20:20:16 +0600

    stripe key added done

--- CLIENT/src/payment/PaymentPage.jsx
+++ CLIENT/src/payment/PaymentPage.jsx
@@ -2,10 +2,22 @@
 
 
 import React from 'react'
+import {CheckoutProvider} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
+
+
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_API_KET)
 function PaymentPage() {
   return (
-    <div>PaymentPage</div>
+    <div>
+       
+
+
+
+
+    </div>
   )
 }
 
