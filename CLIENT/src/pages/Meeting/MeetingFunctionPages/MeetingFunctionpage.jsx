e428d11d178b9a28817f47e387459e5f3efcbc4c
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-28 21:10:07 +0600

    checkand email added done

--- CLIENT/src/pages/Meeting/MeetingFunctionPages/MeetingFunctionpage.jsx
+++ CLIENT/src/pages/Meeting/MeetingFunctionPages/MeetingFunctionpage.jsx
@@ -163,6 +163,7 @@ const MeetingFunctionpage = () => {
   };
 
   // jai time a submit button click kora hobba oi time zoom id send korta hobba
+  ///  meeting time - now === 1 -> send data a components -> then send email
 
   return (
     <div className="container mx-auto ">
@@ -236,11 +237,17 @@ const MeetingFunctionpage = () => {
                         .filter((schedule) => {
                           const now = new Date();
                           const meetingTime = new Date(`${schedule.Date} ${schedule.Time}`);
+
+                         
+
+
+
                           return meetingTime > now;
                         })
                         .sort((a, b) => {
                           const meetingTimeA = new Date(`${a.Date} ${a.Time}`);
                           const meetingTimeB = new Date(`${b.Date} ${b.Time}`);
+                          /// if (nowtime-meetingtime ar gap ===1) send email
                           return meetingTimeA - meetingTimeB; // Sort by Date and Time (low to high)
                         })
                         .map((schedule, index) => (
