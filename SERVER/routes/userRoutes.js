05c6d787c45e128326472c0f1c097f384071cf93
masud2005 <masud20012005@gmail.com>
2025-05-01 13:00:51 +0600

    User route updated

--- SERVER/routes/userRoutes.js
+++ SERVER/routes/userRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (usersCollections) => {
-  router.post('/', async (req, res) => {
+  router.post('/users', async (req, res) => {
     const user = req.body;
     const result = await usersCollections.insertOne(user);
     res.send(result);
   });
 
-  router.get('/', async (req, res) => {
+  router.get('/users', async (req, res) => {
     const results = await usersCollections.find().toArray();
     res.send(results);
   });
