750a45a0694fac26e83ae8cf88d8c110ab371117
masud2005 <masud20012005@gmail.com>
2025-05-01 16:02:18 +0600

    User plan updated APIs added

--- SERVER/routes/userRoutes.js
+++ SERVER/routes/userRoutes.js
@@ -36,5 +36,20 @@ module.exports = (usersCollections) => {
     res.send(result);
   })
 
+  router.patch('/users/plan/:email', async (req, res) => {
+    const email = req.params.email;
+    const { plan } = req.body;
+
+    const filter = { email: email };
+    const updateDoc = {
+      $set: {
+        plan: plan,
+      },
+    };
+
+    const result = await usersCollections.updateOne(filter, updateDoc);
+    res.send(result);
+  })
+
   return router;
 };
